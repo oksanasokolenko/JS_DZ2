3108b54aa9cf3dd88d6260668ba6ffb9d69e9cb8
Oksana Sokolenko <555sova1988@gmail.com>
2017-09-12 20:31:58 +0300

    1

--- часть 1/ задача1/1.js
+++ часть 1/ задача1/1.js
@@ -1,40 +1,17 @@
 
 
 
-// Задача 5
-
-// document.getElementById('start').onclick = f2;
-
-// function f2 () {
-//
-//     var a = document.getElementById('d1').value;
-//     var b = 2017 - a;
-//     if(b==29 && b==15){
-//         alert ("Welcome");
-//     }
-//     else {
-//         alert ("Wrong walue");
-//     }
-// }
-
-
 
 document.getElementById('start').onclick = f2;
 
-function f2 () {
+function f2() {
 
     var a = document.getElementById('d1').value;
-    var b = 2017 - a;
-    if( b == 29 ){
-        alert (" you are 29 y.o. ");
-    }
-    else if (b == 15){
-        alert ( " you are 15 y.o. ");
-    }
-    else {
-        alert ("wrong value");
-    }
+    var b = parseInt(2017) - a;
+    alert ( "you are " + b + " years old")
+
 }
+
 
 
 
